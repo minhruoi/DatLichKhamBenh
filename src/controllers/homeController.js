2118bc396b02fd0a7254e054f46b0a92df1d6374
minhruoi <nguyenngocminha8@gmail.com>
2025-07-02 10:10:56 +0700

    create db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,16 @@
+import db from "../models/index"
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
 
-let getHomePage = (req, res) => {
-    return res.render("homepage.ejs");
+        return res.render("homepage.ejs", {
+            data: JSON.stringify(data)
+        });
+    } catch (exception) {
+        console.log(exception);
+    }
+
+
 }
 
 let getAboutMe = (req, res) => {
